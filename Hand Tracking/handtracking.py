513bb06b3370332513ce361f26fe114b3b4f8a7e
Joel Ajay <ajaijoel@gmail.com>
2025-04-01 12:39:12 +0530

    fix: created an venv

--- Hand Tracking/handtracking.py
+++ Hand Tracking/handtracking.py
@@ -14,6 +14,7 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = hands.process(image)
+        print(results)
 
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
